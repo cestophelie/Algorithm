b08572d2f2043eea782ab723ad82220a37ac3c0b
Ophelie <51237953+cestophelie@users.noreply.github.com>
2021-01-30 15:22:21 +0900

    Create bigger_num_rule.py

---
+++ Study_session/Shortest_path/bigger_num_rule.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    n, m, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    arr.sort(reverse=True)
+    # print('arr : '+str(arr))
+    total = 0
+    for i in range(m//k):
+        for j in range(k):
+            total += arr[0]
+            i += 1
+
+        total += arr[1]
+
+    print('total : '+str(total))
+'''
+5 8 3
+2 4 5 4 6
+'''
